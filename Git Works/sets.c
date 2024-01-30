1fdc2aed9cdba7f2317fbd237267771049d7b158
Guglielmo Gabriele <64373907+Marshmallowbaka@users.noreply.github.com>
2024-01-30 16:07:41 +0100

    Add files via upload

---
+++ Git Works/sets.c
@@ -0,0 +1,100 @@
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+
+#define HASHMAP_SIZE 64
+#define HASHMAP_SIZE_LIST 1
+
+struct set_item
+{
+    const char *key;
+    size_t key_len;
+};
+
+struct set
+{
+    struct set_item hashmap[HASHMAP_SIZE][HASHMAP_SIZE_LIST];
+};
+
+size_t djb33x_hash(const char *key, const size_t keylen)
+{
+    size_t hash = 5381;
+
+    for (size_t i = 0; i < keylen; i++)
+    {
+        hash = ((hash << 5) + hash) ^ key[i];
+    }
+
+    return hash;
+}
+
+void set_insert(struct set *set, const char *key)
+{
+    const size_t key_len = strlen(key);
+
+    const size_t hash = djb33x_hash(key, key_len);
+
+    const size_t index = hash % HASHMAP_SIZE;
+
+    printf("hash of %s = %llu (index: %llu)\n", key, hash, index);
+
+    for (size_t i = 0; i < HASHMAP_SIZE_LIST; i++)
+    {
+        if (set->hashmap[index][i].key_len == 0)
+        {
+            set->hashmap[index][i].key = key;
+            set->hashmap[index][i].key_len = key_len;
+            printf("added %s at index %llu slot %llu\n", key, index, i);
+            return;
+        }
+    }
+
+    printf("COLLISION! for %s (index %llu)\n", key, index);
+}
+
+void set_find(struct set *set, const char *key)
+{
+    const size_t key_len = strlen(key);
+
+    const size_t hash = djb33x_hash(key, key_len);
+
+    const size_t index = hash % HASHMAP_SIZE;
+
+    printf("hash of %s = %llu (index: %llu)\n", key, hash, index);
+
+    for (size_t i = 0; i < HASHMAP_SIZE_LIST; i++)
+    {
+        if (set->hashmap[index][i].key_len > 0)
+        {
+            if (set->hashmap[index][i].key_len == key_len && !memcmp(set->hashmap[index][i].key, key, key_len))
+            {
+                printf("FOUND %s at index %llu slot %llu\n", key, index, i);
+                return;
+            }
+        }
+    }
+}
+
+void set_remove(struct set *set, const char *key)
+{
+    const size_t key_len = strlen(key);
+
+    const size_t hash = djb33x_hash(key, key_len);
+
+    const size_t index = hash % HASHMAP_SIZE;
+
+    for (size_t i = 0; i < HASHMAP_SIZE_LIST; i++)
+    {
+        if (set->hashmap[index][i].key_len > 0)
+        {
+            if (set->hashmap[index][i].key_len == key_len && !memcmp(set->hashmap[index][i].key, key, key_len))
+            {
+                set->hashmap[index][i].key_len = NULL;
+                set->hashmap[index][i].key = NULL;
+                free(set->hashmap[index][i]);
+                printf("REMOVED %s at index %llu slot %llu\n", key, index, i);
+                return;
+            }
+        }
+    }
+}
